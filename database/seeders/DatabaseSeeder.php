6e5e7d64100a747f28fccc354166d47748a60f36
Gabi Vilaplana <gabired345@gmail.com>
2026-01-15 20:22:45 +0100

    5.3 Terminado

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,20 +6,14 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // Des d'ací cridem la resta de seeders
         $this->call([
             EstadisSeeder::class,
             EquipsSeeder::class,
+            UserSeeder::class,
             JugadorasSeeder::class,
-            PartitsSeeder::class,
+            PartitsSeeder::class
         ]);
-
-        // Opcional: per veure que acaba
-        dump('DatabaseSeeder: FIN');
     }
 }
